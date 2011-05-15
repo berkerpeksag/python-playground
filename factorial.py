15e0e6f738e7a530ea5e1aac1b4269741a401677
Berker Peksag <berker.peksag@gmail.com>
2011-05-15 14:56:07 +0300

    Added factorial example.

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def factorial(n):
+    r = 1
+
+    for i in reversed(range(1, n + 1)):
+        r *= i
+
+    return r
+
+if __name__ == '__main__':
+    print factorial(5)
+    print factorial(1)
+    print factorial(0)
+    print factorial(10)
+    print factorial(-3)
