58a3477041ebedc5cee2f32ebec94c6f9398d4a2
Berker Peksag <berker.peksag@gmail.com>
2011-05-19 16:13:44 +0300

    Added 1-28.

---
+++ divide.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+The Algorithm Design Manual by Steven S. Skiena
+
+1-28. [5] Write a function to perform integer division without using
+either the / or + operators. Find a first way to do it.
+"""
+
+def divide(numerator, denominator):
+    quotient = 0
+
+    while numerator >= denominator:
+        numerator -= denominator
+        quotient += 1
+
+    return quotient
+
+
+if __name__ == '__main__':
+    print divide(4, 2)
+    print divide(6, 3)
+    print divide(10, 5)
+    print divide(50, 10)
