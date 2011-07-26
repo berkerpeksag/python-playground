1a349cc680acd49d047aac93d5e86835966a2913
Berker Peksag <berker.peksag@gmail.com>
2011-07-27 00:16:22 +0300

    Fixed map example.

--- map.py
+++ map.py
@@ -4,7 +4,9 @@
 def add(x, y):
     return x + y
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [6, 7, 8, 9, 10]
+if __name__ == '__main__':
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [6, 7, 8, 9, 10]
 
-lists_sum = map(add, list1, list2)
+    lists_sum = map(add, list1, list2)
+    print lists_sum
