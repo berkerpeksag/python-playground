2e5ccf4574ef730bbeeae2763776b2783b6a2f6e
Berker Peksag <berker.peksag@gmail.com>
2011-07-26 18:18:14 +0300

    Added debug string for tail call example.

--- fibonacci_with_tail_call.py
+++ fibonacci_with_tail_call.py
@@ -5,7 +5,8 @@ def fibonacci(n, current = 0, next = 1):
     if n == 0:
         return current
     else:
+        print 'fibonacci(%d - 1, %d, %d + %d)' % (n - 1, next, current, next)
         return fibonacci(n - 1, next, current + next)
 
 if __name__ == '__main__':
-    print fibonacci(100)
+    print fibonacci(10)
