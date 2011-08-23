3293e42f3de2d6d7b2242db7c147bea612f25347
Berker Peksag <berker.peksag@gmail.com>
2011-08-23 22:47:36 +0300

    Added package example.

---
+++ mika/mika/mika.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+class Hede(object):
+    def __init__(self):
+        print 'a.k.'
+
+    def hodo(self):
+        print 'hodo'
---
+++ mika/test.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+from mika import mika
+
+
+class A(mika.Hede):
+    def ak(self):
+        print 'ak'
+
+A()
