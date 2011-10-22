f652b18ed7f7f4f5a4bfc1b7c7cf6849aba24c94
Berker Peksag <berker.peksag@gmail.com>
2011-10-23 00:56:24 +0300

    Added __getattr__ example.

---
+++ getattr.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+class A(object):
+    def __getattr__(self, name):
+        if name.startswith('print_'):
+            return object.__getattribute__(self, name[6:])
+
+        return object.__getattribute__(self, name)
+
+    def hi(self):
+        print 'Hello!'
+
+a = A()
+
+a.hi()
+a.print_hi()
