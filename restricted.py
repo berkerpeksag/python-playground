9d4be6089719a4b85a2ad0efca244a9354f0ad17
Berker Peksag <berker.peksag@gmail.com>
2012-09-10 23:08:27 +0300

    Added an example to manipulate __builtins__ in Python 2.7.
    - Execution of restricted Python code.
    - Usage: python restricted.py evil.py

---
+++ restricted.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+"""
+Execution of restricted Python code.
+
+Use with `evil.py`.
+
+https://gist.github.com/3437909
+"""
+
+import platform
+import pprint
+import os
+import os.path
+import sys
+
+
+def _restrict(filename, debug=False):
+    # always add a newline, compile needs one in Python < 2.7.
+    source = open(filename, 'r').read() + '\n'
+
+    code = compile(source, filename, 'exec')
+    global_ns = {
+        '__builtins__': {
+            'False': False,
+            'None': None,
+            'True': True,
+        },
+        'uname': platform.uname(),
+    }
+    local_ns = {}
+    exec code in global_ns, local_ns
+
+    if debug:
+        print 'Globals'
+        pprint.pprint(global_ns)
+        print 'Locals'
+        pprint.pprint(local_ns)
+
+
+def main(filename):
+    filename = os.path.abspath(os.path.normpath(os.path.expandvars(
+        os.path.expanduser(filename))))
+    if not os.path.isfile(filename):
+        raise IOError('File not found: {0:s}'.format(filename))
+
+    _restrict(filename, debug=True)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
