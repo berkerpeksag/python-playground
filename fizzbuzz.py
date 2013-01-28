d92eaadd670ca0679be43292336b7859946621ee
Berker Peksag <berker.peksag@gmail.com>
2013-01-28 19:51:10 +0200

    Add fizbuzz example.

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+def main():
+    for num in xrange(1, 101):
+        msg = ''
+        if num % 3 == 0:
+            msg += 'Fizz'
+        if num % 5 == 0:
+            msg += 'Buzz'
+        print msg or num
+
+if __name__ == '__main__':
+    main()
