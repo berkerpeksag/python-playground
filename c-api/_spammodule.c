12f2a8c4da75279cd780480b68e62c4000471061
Berker Peksag <berker.peksag@gmail.com>
2013-02-18 22:37:23 +0200

    Add a CPython C API example.

---
+++ c-api/_spammodule.c
@@ -0,0 +1,33 @@
+#include <Python.h>
+
+static PyObject *
+spam_strptime(PyObject *self, PyObject *args)
+{
+    const char *date_string;
+    const char *format;
+
+    if (!PyArg_ParseTuple(args, "ss", &date_string, &format))
+        return NULL;
+
+    return Py_BuildValue("ss", date_string, format);
+}
+
+static PyMethodDef SpamMethods[] = {
+    {"strptime", spam_strptime, METH_VARARGS,
+     "Return a parsed date object."},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef spammodule = {
+    PyModuleDef_HEAD_INIT,
+    "spam",
+    "doc",
+    -1,
+    SpamMethods
+};
+
+PyMODINIT_FUNC
+PyInit_spam(void)
+{
+    return PyModule_Create(&spammodule);
+}
