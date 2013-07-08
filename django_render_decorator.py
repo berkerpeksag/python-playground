633d93d0fb52c3f7b6cef297c7dd5bb23d8dc1e3
Berker Peksag <berker.peksag@gmail.com>
2013-07-08 21:12:31 +0300

    Document the render decorator.

--- django_render_decorator.py
+++ django_render_decorator.py
@@ -3,6 +3,16 @@ from django.template import RequestContext
 
 
 def render(template_name):
+    """
+    A decorator to render the view for *template_name*.
+
+    To use::
+
+        @render('foo')
+        def index(request):
+            return {'spam': 'eggs'}
+
+    """
     def outer(func):
         def inner(request, *args, **kwargs):
             return render_to_response('{0:s}.html'.format(template_name),
