3e925a1d7977aa16ba794201811f869a8e9173c0
Berker Peksag <berker.peksag@gmail.com>
2013-07-08 21:14:00 +0300

    Fix a typo.

--- tokenizing.py
+++ tokenizing.py
@@ -5,7 +5,7 @@ import tokenize
 def main(filename):
     reader = open(filename, 'r').readline
     for typ, tok, (sr, sc), (er, ec), line in tokenize.generate_tokens(reader):
-        print type, tok, (sr, sc)
+        print typ, tok, (sr, sc)
 
 if __name__ == '__main__':
     main(sys.argv[1])
