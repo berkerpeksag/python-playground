967c3ddf074cf86e796ee128f4ebab141aab12f1
Berker Peksag <berker.peksag@gmail.com>
2014-03-10 09:23:20 +0200

    Delete descriptor_funcs_meths.py

--- descriptor_funcs_meths.py
+++
@@ -1,8 +0,0 @@
-import types
-
-
-class Function(object):
-    # ...
-    def __get__(self, obj, objtype=None):
-        """Simulate func_descr_get() in Objects/funcobject.c"""
-        return types.MethodType(self, obj, objtype)
