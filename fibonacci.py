9651d3ff1745b322031dd36e8dc7f0dc9d8a7ac9
Berker Peksag <berker.peksag@gmail.com>
2014-03-12 12:04:45 +0200

    Cleanup fibonacci examples.

--- fibonacci.py
+++ fibonacci.py
@@ -1,22 +1,43 @@
-#!/usr/bin/env python
-# coding: utf-8
+from __future__ import print_function
+
+from memoize import memoize
 
 
-def fibonacci(n):
-    a, b = 0, 1  # a = 0 and b = 1
+def fib1(n):
+    """Iterative Fibonacci example."""
+    a, b = 0, 1
+    for i in range(0, n):
+        a, b = b, a + b
+    return a
 
-    while a < n:
-        print a,
 
-        a, b = b, a + b  # a = b and b = a + b
+def fib2(n):
+    """Fibonacci example *without* tail call optimization."""
+    if n < 2:
+        return n
+    else:
+        return fib2(n - 1) + fib2(n - 2)
 
 
-def main():
-    print fibonacci(5)
-    print fibonacci(1)
-    print fibonacci(0)
-    print fibonacci(10)
-    print fibonacci(3)
+def fib3(n, current=0, next_item=1):
+    """Fibonacci example *with* tail call optimization."""
+    if n == 0:
+        return current
+    else:
+        return fib3(n - 1, next_item, current + next_item)
+
+
+fib4 = memoize(fib2)
+fib4.__doc__ = 'Fibonacci example with the memoization pattern.'
+
 
 if __name__ == '__main__':
-    main()
+    for fib in (fib1, fib3, fib4, fib2):
+        print(fib.__doc__)
+        print(fib(5))
+        print(fib(1))
+        print(fib(0))
+        print(fib(10))
+        print(fib(3))
+        print(fib(20))
+        print(fib(100))
