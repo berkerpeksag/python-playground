7d524aaadcb1e88b1980f5d91679f2799b0f6706
Berker Peksag <berker.peksag@gmail.com>
2014-10-02 23:23:41 +0300

    Tweak auto enum example a bit.

--- auto_enum.py
+++ auto_enum.py
@@ -5,12 +5,11 @@ import itertools
 class AutoEnum(type):
 
     @classmethod
-    def __prepare__(metacls, name, bases):
+    def __prepare__(mcls, name, bases):
         return collections.defaultdict(itertools.count().__next__)
 
     def __new__(cls, name, bases, classdict):
-        result = type.__new__(cls, name, bases, dict(classdict))
-        return result
+        return type.__new__(cls, name, bases, dict(classdict))
 
 
 class Enum(metaclass=AutoEnum):
