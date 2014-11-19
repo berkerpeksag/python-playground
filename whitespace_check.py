69506eccdbd05297b6ef11fb8cbda7071a2ab887
Berker Peksag <berker.peksag@gmail.com>
2014-11-19 10:57:41 +0200

    Tweak whitespace_check.

--- whitespace_check.py
+++ whitespace_check.py
@@ -4,10 +4,12 @@ import string
 
 
 def check_whitespace(strn):
-    return any(map(lambda s: s in string.whitespace, strn))
+    return any(s in string.whitespace for s in strn)
 
 if __name__ == '__main__':
-    tests = ('berker', 'berker peksag', '  berker', 'berker ',
-             '\tber\n',)
+    tests = (
+        'berker', 'berker peksag', '  berker', 'berker ',
+        '\tber\n',
+    )
     for test in tests:
         print(test, check_whitespace(test))
