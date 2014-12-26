b2bf21578f588dfaa5bbfdbada96be86a152e5cf
Berker Peksag <berker.peksag@gmail.com>
2014-12-26 23:20:12 +0200

    Cosmetic update.

--- c-api/setup.py
+++ c-api/setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup, Extension
 spammodule = Extension('spam',
                        sources=['_spammodule.c'])
 
-setup(name='Spam',
-      version='1.0',
-      description='This is a demo module.',
-      ext_modules=[spammodule])
+setup(
+    name='Spam',
+    version='1.0',
+    description='This is a demo module.',
+    ext_modules=[spammodule],
+)
