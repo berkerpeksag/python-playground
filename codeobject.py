b61a68a557e57608d1dc2f29a2b030b71e638f96
Berker Peksag <berker.peksag@gmail.com>
2015-01-01 03:52:35 +0200

    Tweak codeobject example a bit.

--- codeobject.py
+++ codeobject.py
@@ -1,18 +1,16 @@
-# coding: utf-8
+from __future__ import print_function
+
 
 def func(x=42):
     y = x * 3
     return y
 
 
-def main():
-    code_obj = func.func_code
-    print code_obj.co_name
-    print code_obj.co_filename
-    print code_obj.co_argcount
-    print code_obj.co_varnames
-    print code_obj.co_consts
-    print code_obj.co_code
-
 if __name__ == '__main__':
-    main()
+    code_obj = func.func_code
+    print('co_name', code_obj.co_name)
+    print('co_filename', code_obj.co_filename)
+    print('co_argcount', code_obj.co_argcount)
+    print('co_varnames', code_obj.co_varnames)
+    print('co_consts', code_obj.co_consts)
+    print('co_code', repr(code_obj.co_code))
