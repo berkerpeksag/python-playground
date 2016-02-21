87ac0fe3be9604701e51c4d8b6afec7498cc54fe
Berker Peksag <berker.peksag@gmail.com>
2016-02-21 02:51:19 +0200

    Cleanup preforking example a bit

--- preforking.py
+++ preforking.py
@@ -1,34 +1,26 @@
 import os
 import socket
-import signal
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 9999))
-s.listen(1)
-
-wpids = []
-
-
-def handler(signum, frame):
-    for pid in wpids:
-        os.kill(pid, signum)
-
-for sig in [signal.SIGINT, signal.SIGQUIT]:
-    signal.signal(sig, handler)
+s.listen(10)
 
 for _ in range(5):
     pid = os.fork()
-    if pid != 0:
-        wpids.append(pid)
-        while True:
-            conn, addr = s.accept()
-            conn.sendall(b"Hello Readers!\n")
-            conn.close()
+    if pid == 0:
+        cpid = os.getpid()
+        try:
+            while True:
+                conn, addr = s.accept()
+                conn.sendall(b"Hello Readers!\n")
+                conn.close()
+        except KeyboardInterrupt:
+            print('bailing child {}...'.format(cpid))
+            sys.exit()
 
 try:
-    while True:
-        wpid, _ = os.waitpid(-1, os.WNOHANG)
-        if not wpid:
-            break
-except ChildProcessError:
-    pass
+    os.waitpid(-1, 0)
+except KeyboardInterrupt:
+    print("bailing...")
+    sys.exit()
